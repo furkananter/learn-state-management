788864f23a311dc83d0390c54b95c7cbd65df21e
Merovingian <52575351+furkananter@users.noreply.github.com>
2023-02-28 23:08:20 +0300

    part3

--- socialapp-redux-tutorial/src/app/store.js
+++ socialapp-redux-tutorial/src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
 
+
+console.log('postsReducer', postsReducer)
+
 export default configureStore({
   reducer: {
     posts: postsReducer,

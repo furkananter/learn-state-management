36601a69af73d54f6ca2e367db95f58961c06435
Merovingian <52575351+furkananter@users.noreply.github.com>
2023-03-06 08:47:43 +0300

    userslist

---
+++ socialapp-redux-tutorial/src/features/users/UserPage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectAllUsers, selectUserById } from './usersSlice'
+import { selectAllPosts } from '../posts/postsSlice'
+import { Link } from 'react-router-dom'
+
+const UserPage = ({ match }) => {
+  const { userId } = match.params
+
+  const user = useSelector((state) => selectUserById(state, userId))
+
+  const postForUser = useSelector((state) => {
+    const allPosts = selectAllPosts(state) // burada tüm postları alıyoruz.
+    return allPosts.filter((post) => post.user === userId) // burada da tüm postları dolaşıp, userId ile eşleşenleri alıyoruz.
+  })
+
+  const postTitles = postForUser.map((post) => (
+    <li key={post.id}>
+      <Link to={`/posts/${post.id}`}>{post.title}</Link>
+    </li>
+  ))
+
+  return (
+    <section>
+      <h2>{user.name}</h2>
+      <ul>{renderedUsers}</ul>
+    </section>
+  )
+}
+
+export default UserPage
---
+++ socialapp-redux-tutorial/src/features/users/UsersList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectAllUsers } from './usersSlice'
+
+const UsersList = () => {
+  const users = useSelector(selectAllUsers)
+  const renderedUsers = users.map((user) => {
+    return (
+      <li key={user.id}>
+        <Link to={`/users/${user.id}`}>
+            {user.name}
+        </Link>
+      </li>
+    )
+  })
+
+  return (
+    <section>
+      <h2>Users</h2>
+      <ul>{renderedUsers}</ul>
+    </section>
+  )
+}
+
+export default UsersList
